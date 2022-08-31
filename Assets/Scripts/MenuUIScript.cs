668e08f585b6c939b774bc43989414320d4b1f02
JWatts1402 <JonathanWatts1402@gmail.com>
2022-08-31 15:52:31 +0100

    menu code
    added functionality to main menu scene
    can transition between the main menu, settings menu, and game mode option menu using buttons

--- Assets/Scripts/MenuUIScript.cs
+++ Assets/Scripts/MenuUIScript.cs
@@ -7,7 +7,9 @@ public class MenuUIScript : MonoBehaviour
 {
 
     SceneManager sceneManager = new SceneManager();
-
+    public GameObject MainMenuPanel;
+    public GameObject SettingsPanel;
+    public GameObject GameModePanel;
 
     public void SingleplayerLevelSelect()
     {
@@ -15,13 +17,31 @@ public class MenuUIScript : MonoBehaviour
         //enable level select screen
     }
 
+    public void LoadGameModePanel()
+    {   
+        GameModePanel.SetActive(true);
+        MainMenuPanel.SetActive(false);
+    }
+    public void GameModeBacktoMenuButton()
+    {
+        MainMenuPanel.SetActive(true);
+        GameModePanel.SetActive(false);
+    }
+
+    public void SettingsBacktoMenuButton()
+    {
+        MainMenuPanel.SetActive(true);
+        SettingsPanel.SetActive(false);
+    }
+
     public void SettingsScreenLoad()
     {
-        //temporarily disable main menu panel
-        //enable settings panel
+        SettingsPanel.SetActive(true);
+        MainMenuPanel.SetActive(false);
     }
     public void QuitButton()
     {
+        Debug.Log("game quit");
         Application.Quit();
     }
 
