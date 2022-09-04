9acad906186598db7f666613f4798f501bb48b76
Tom Roberts <thomas.roberts3@durham.ac.uk>
2022-09-05 00:28:29 +0100

    Added aim assist
    also can use the static PlayerNetworking.ConnectedPlayers to find the list/dict of all connected players

--- Assets/Scripts/Player/GunController.cs
+++ Assets/Scripts/Player/GunController.cs
@@ -18,6 +18,16 @@ public class GunController : MonoBehaviour
     [Tooltip("Innacuracy of gun. 0 is perfect, 0.25 means 45-degrees of variation, 1 means 360-degrees.")]
     public float innacuracy = 0.01f;
 
+    [Header("Aim Assist Settings")]
+    [Tooltip("Aiming within this amount of degrees to an enemy will snap your aim to them")]
+    public float aimAssistAngle = 4;
+    [Tooltip("The distance at which you get most aim assist")]
+    public float optimalAimAssistDistance = 15f;
+    [Tooltip("The distance at which you stop getting any aim assist")]
+    public float maximumAimAssistDistance = 50f;
+    [Tooltip("The furthest bullet distance you can shoot before the rays no longer hit.")]
+    public float raycastDistance = 100f;
+
     [Header("Animation Settings")]
     [Tooltip("How long in seconds the shooting animation will last")]
     public float animationDuration = 0.8f;
@@ -120,17 +130,99 @@ public class GunController : MonoBehaviour
     }
 
     /// <summary>
-    /// 
+    /// Loops through the players in the scene and checks whether they are within a certain angle of the shooting direction.
+    /// If so, then the shot ray snaps to the player.
     /// </summary>
-    /// <param name="startPos"></param>
-    /// <param name="shootDirection"></param>
+    /// <param name="startPos">Start position of raycast</param>
+    /// <param name="shootDirection">Original direction of raycast</param>
+    /// <param name="hitBool">Returns whether the shot hit anything or not (including with aimassist)</param>
+    /// <param name="hitRaycastReferenceObj">Returns the racast hit information of the chosen ray, if there is any</param>
+    /// <returns></returns>
+    private Vector3 AdjustForAimAssist(Vector3 startPos, Vector3 shootDirection, out bool hitBool, out RaycastHit hitRaycastReferenceObj)
+    {
+        hitBool = Physics.Raycast(startPos, shootDirection, out hitRaycastReferenceObj, raycastDistance);
+
+        Vector3 outShootDirection = shootDirection;
+        // If the player missed, see if aim assist will help
+        if (!hitBool || (hitBool && hitRaycastReferenceObj.collider.gameObject.tag != "Player"))
+        {
+            // Setup the variables to write to
+            float closestAngle = float.MaxValue;
+            Vector3 closestShootDirection = Vector3.zero;
+            bool foundTarget = false;
+
+            // Find the list of player gameobjects that are currently in the game
+            Dictionary<ulong, GameObject>.ValueCollection inGameTargets = PlayerNetworking.ConnectedPlayers.Values;
+            
+            // Loop through the "targets", aka the other players in the game
+            foreach (GameObject target in inGameTargets)
+            {
+                // Find the rigidbody attatched to the target in the most efficient way possible
+                PlayerNetworking targetPlayerNetworking;
+                if(target && target.TryGetComponent<PlayerNetworking>(out targetPlayerNetworking))
+                {
+                    Vector3 targetPos = targetPlayerNetworking.bodyRigidbody.position;
+
+                    // Get direction to the target person
+                    Vector3 shootDirectionToTarget = (targetPos - startPos).normalized;
+                    // Check angle is within aim assist angle
+                    float angle = Vector3.Angle(shootDirection, shootDirectionToTarget);
+                    float distance = Vector3.Distance(startPos, targetPos);
+                    float angleMultiplier = 1;
+
+                    // Increase the aim assist angle as distance increases towards the optimal distance
+                    if (distance < optimalAimAssistDistance)
+                    {
+                        angleMultiplier = Mathf.Clamp01(distance / optimalAimAssistDistance);
+                        if (angleMultiplier < 0.2f)
+                            angleMultiplier = 0.2f;
+                    }
+
+                    // Linearly decrease the aim assist angle after the optimal distance
+                    if (distance > optimalAimAssistDistance)
+                        angleMultiplier = 1 - Mathf.Clamp01((distance - optimalAimAssistDistance) / (maximumAimAssistDistance - optimalAimAssistDistance));
+
+                    if (angle < aimAssistAngle * angleMultiplier)
+                    {
+                        // If within angle, see if raycast would hit
+                        RaycastHit targetRaycastHit;
+                        bool targetHit = Physics.Raycast(startPos, shootDirectionToTarget, out targetRaycastHit, raycastDistance);
+                        if (targetHit)
+                        {
+                            // If target is hittable, see if it's the closest one to the mouse pointer
+                            foundTarget = true;
+                            if (angle < closestAngle)
+                            {
+                                closestAngle = angle;
+                                closestShootDirection = shootDirectionToTarget;
+                                hitRaycastReferenceObj = targetRaycastHit;
+                            }
+                        }
+                    }
+                }
+            }
+            if (foundTarget)
+            {
+                outShootDirection = closestShootDirection;
+                hitBool = true;
+            }
+        }
+
+        return outShootDirection;
+    }
+
+    /// <summary>
+    /// Animates shooting the gun from a particlar start position and direction
+    /// </summary>
     /// <returns>The gameobject that was shot. Is null if nothing is shot</returns>
     public GameObject Shoot(Vector3 startPos, Vector3 shootDirection)
     {
-        // Add random spread
-        RaycastHit hitRaycastReferenceObj;
+
         GameObject hitGameObject = null;
-        bool hit = Physics.Raycast(startPos, shootDirection, out hitRaycastReferenceObj, 100.0f);
+        RaycastHit hitRaycastReferenceObj;
+        bool hit;
+
+        Vector3 shootDirectionAfterAimAssist = AdjustForAimAssist(startPos, shootDirection, out hit, out hitRaycastReferenceObj);
 
         GameObject myLine = new GameObject();
         myLine.transform.position = muzzlePoint.position;
@@ -169,7 +261,7 @@ public class GunController : MonoBehaviour
         }
         else
         {
-            lr.SetPosition(1, Camera.main.transform.position + shootDirection * 100f);
+            lr.SetPosition(1, Camera.main.transform.position + shootDirectionAfterAimAssist * raycastDistance);
         }
         audioManager.Shoot();
         GameObject muzzle_particle = Instantiate(muzzleFlashParticlePrefab, muzzlePoint.position, muzzlePoint.rotation);//
