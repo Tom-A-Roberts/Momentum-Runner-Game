111e52c6e7d82315d9d142a669674004c934e234
JWatts1402 <JonathanWatts1402@gmail.com>
2022-09-13 14:10:12 +0100

    changed target to use shoot interface

--- Assets/Scripts/Interactive Map Entities/Target.cs
+++ Assets/Scripts/Interactive Map Entities/Target.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 
 
 
-public class Target : MonoBehaviour
+public class Target: MonoBehaviour , IShootInterface
 {
-    public GameObject Door;
+    public GameObject[] ObjectsToChange;
 
-    public void TargetHit()
+    void IShootInterface.OnHitByLaser()
     {
-        Door.SetActive(false);
+       foreach (GameObject gb in ObjectsToChange)
+        {
+            gb.SetActive(!gb.activeSelf);
+        }
     }
-
 }
